257c48bef891c8122fe895e9f0fffda4f976f198
CollierTR <tristancollier777@gmail.com>
2026-04-21 22:34:18 -0500

    add markdown parser and tests

---
+++ src/markdown_parser.py
@@ -0,0 +1,24 @@
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if not old_nodes:
+        return []
+
+    new_nodes = []
+
+
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN_TEXT:
+            new_nodes.append(node)
+            continue
+        split_nodes = node.text.split(delimiter)
+        if len(split_nodes) % 2 == 0:
+            raise Exception(f"Invalid Markdown! Unclosed '{delimiter}'!")
+        else:
+            for i, split in enumerate(split_nodes):
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(split, TextType.PLAIN_TEXT))
+                else:
+                    new_nodes.append(TextNode(split, text_type))
+
+    return new_nodes
---
+++ src/text_markdown_parser.py
@@ -0,0 +1,94 @@
+import unittest
+
+from textnode import TextNode, TextType
+from markdown_parser import split_nodes_delimiter  # adjust import
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+
+    def test_no_nodes(self):
+        result = split_nodes_delimiter([], "*", TextType.BOLD_TEXT)
+        self.assertEqual(result, [])
+
+    def test_no_delimiter(self):
+        nodes = [TextNode("hello world", TextType.PLAIN_TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD_TEXT)
+
+        expected = [TextNode("hello world", TextType.PLAIN_TEXT)]
+        self.assertEqual(result, expected)
+
+    def test_single_delimiter_pair(self):
+        nodes = [TextNode("hello *world* test", TextType.PLAIN_TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD_TEXT)
+
+        expected = [
+            TextNode("hello ", TextType.PLAIN_TEXT),
+            TextNode("world", TextType.BOLD_TEXT),
+            TextNode(" test", TextType.PLAIN_TEXT),
+        ]
+        self.assertEqual(result, expected)
+
+    def test_multiple_delimiters(self):
+        nodes = [TextNode("a *b* c *d* e", TextType.PLAIN_TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD_TEXT)
+
+        expected = [
+            TextNode("a ", TextType.PLAIN_TEXT),
+            TextNode("b", TextType.BOLD_TEXT),
+            TextNode(" c ", TextType.PLAIN_TEXT),
+            TextNode("d", TextType.BOLD_TEXT),
+            TextNode(" e", TextType.PLAIN_TEXT),
+        ]
+        self.assertEqual(result, expected)
+
+    def test_unclosed_delimiter_raises(self):
+        nodes = [TextNode("hello *world test", TextType.PLAIN_TEXT)]
+
+        with self.assertRaises(Exception):
+            split_nodes_delimiter(nodes, "*", TextType.BOLD_TEXT)
+
+    def test_non_plain_text_passthrough(self):
+        nodes = [TextNode("hello *world*", TextType.BOLD_TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.ITALIC_TEXT)
+
+        # Should not be modified
+        self.assertEqual(result, nodes)
+
+    def test_mixed_nodes(self):
+        nodes = [
+            TextNode("hello *world*", TextType.PLAIN_TEXT),
+            TextNode("already bold", TextType.BOLD_TEXT),
+        ]
+
+        result = split_nodes_delimiter(nodes, "*", TextType.ITALIC_TEXT)
+
+        expected = [
+            TextNode("hello ", TextType.PLAIN_TEXT),
+            TextNode("world", TextType.ITALIC_TEXT),
+            TextNode("", TextType.PLAIN_TEXT),  # depends if you skip empty
+            TextNode("already bold", TextType.BOLD_TEXT),
+        ]
+
+        # If you skip empty strings, remove that expected node
+        self.assertEqual(result, [n for n in expected if n.text != ""])
+
+    def test_adjacent_delimiters(self):
+        nodes = [TextNode("hello **world**", TextType.PLAIN_TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD_TEXT)
+
+        # This exposes behavior with empty splits
+        # Depending on implementation, you may skip empty strings
+        expected = [
+            TextNode("hello ", TextType.PLAIN_TEXT),
+            TextNode("", TextType.BOLD_TEXT),
+            TextNode("world", TextType.PLAIN_TEXT),
+            TextNode("", TextType.BOLD_TEXT),
+            TextNode("", TextType.PLAIN_TEXT),
+        ]
+
+        # Adjust depending on whether you filter empty strings
+        self.assertEqual(result, [n for n in expected if n.text != ""])
+
+
+if __name__ == "__main__":
+    unittest.main()
